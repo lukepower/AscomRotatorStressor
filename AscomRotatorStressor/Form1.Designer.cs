d8739db89ddf04f8742bc68c699e8b8e26b84dca
Lukas Demetz <lukas@lukepower.com>
2025-01-09 21:31:55 +0100

    Projektdateien hinzufügen.

---
+++ AscomRotatorStressor/Form1.Designer.cs
@@ -0,0 +1,157 @@
+﻿namespace ASCOM.AscomRotatorTorture
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.buttonChoose = new System.Windows.Forms.Button();
+            this.buttonConnect = new System.Windows.Forms.Button();
+            this.labelDriverId = new System.Windows.Forms.Label();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // buttonChoose
+            // 
+            this.buttonChoose.Location = new System.Drawing.Point(309, 10);
+            this.buttonChoose.Name = "buttonChoose";
+            this.buttonChoose.Size = new System.Drawing.Size(72, 23);
+            this.buttonChoose.TabIndex = 0;
+            this.buttonChoose.Text = "Choose";
+            this.buttonChoose.UseVisualStyleBackColor = true;
+            this.buttonChoose.Click += new System.EventHandler(this.buttonChoose_Click);
+            // 
+            // buttonConnect
+            // 
+            this.buttonConnect.Location = new System.Drawing.Point(309, 39);
+            this.buttonConnect.Name = "buttonConnect";
+            this.buttonConnect.Size = new System.Drawing.Size(72, 23);
+            this.buttonConnect.TabIndex = 1;
+            this.buttonConnect.Text = "Connect";
+            this.buttonConnect.UseVisualStyleBackColor = true;
+            this.buttonConnect.Click += new System.EventHandler(this.buttonConnect_Click);
+            // 
+            // labelDriverId
+            // 
+            this.labelDriverId.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.labelDriverId.DataBindings.Add(new System.Windows.Forms.Binding("Text", global::ASCOM.AscomRotatorTorture.Properties.Settings.Default, "DriverId", true, System.Windows.Forms.DataSourceUpdateMode.OnPropertyChanged));
+            this.labelDriverId.Location = new System.Drawing.Point(12, 40);
+            this.labelDriverId.Name = "labelDriverId";
+            this.labelDriverId.Size = new System.Drawing.Size(291, 21);
+            this.labelDriverId.TabIndex = 2;
+            this.labelDriverId.Text = global::ASCOM.AscomRotatorTorture.Properties.Settings.Default.DriverId;
+            this.labelDriverId.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(712, 18);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(172, 17);
+            this.checkBox1.TabIndex = 3;
+            this.checkBox1.Text = "Inhibit Move() if already moving";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(712, 41);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(121, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Start Stressing";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(393, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(86, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Repeat time (ms)";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(497, 14);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 6;
+            this.textBox1.Text = "100";
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(13, 86);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(1012, 511);
+            this.listBox1.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1037, 615);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.labelDriverId);
+            this.Controls.Add(this.buttonConnect);
+            this.Controls.Add(this.buttonChoose);
+            this.Name = "Form1";
+            this.Text = "TEMPLATEDEVICETYPE Test";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonChoose;
+        private System.Windows.Forms.Button buttonConnect;
+        private System.Windows.Forms.Label labelDriverId;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.ListBox listBox1;
+    }
+}
+
---
+++ AscomRotatorStressor/Form1.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace ASCOM.AscomRotatorTorture
+{
+    public partial class Form1 : Form
+    {
+
+        private ASCOM.DriverAccess.Rotator driver;
+        private float start_position;
+        private int count_ticks;
+
+        private int total_calls;
+
+        private bool waiting_for_move;
+
+        public Form1()
+        {
+            InitializeComponent();
+            SetUIState();
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (IsConnected)
+                driver.Connected = false;
+
+            Properties.Settings.Default.Save();
+        }
+
+        private void buttonChoose_Click(object sender, EventArgs e)
+        {
+            Properties.Settings.Default.DriverId = ASCOM.DriverAccess.Rotator.Choose(Properties.Settings.Default.DriverId);
+            SetUIState();
+        }
+
+        private void buttonConnect_Click(object sender, EventArgs e)
+        {
+            if (IsConnected)
+            {
+                driver.Connected = false;
+            }
+            else
+            {
+                driver = new ASCOM.DriverAccess.Rotator(Properties.Settings.Default.DriverId);
+                driver.Connected = true;
+            }
+            SetUIState();
+        }
+
+        private void SetUIState()
+        {
+            buttonConnect.Enabled = !string.IsNullOrEmpty(Properties.Settings.Default.DriverId);
+            buttonChoose.Enabled = !IsConnected;
+            buttonConnect.Text = IsConnected ? "Disconnect" : "Connect";
+        }
+
+        private bool IsConnected
+        {
+            get
+            {
+                return ((this.driver != null) && (driver.Connected == true));
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (this.IsConnected)
+            {
+                // Start stress test
+                if (this.timer1.Enabled == false)
+                {
+                    this.listBox1.Items.Clear();
+                    this.waiting_for_move = false;
+                    this.total_calls = 0;
+                    
+                    this.listBox1.Items.Add(this.driver.Name);
+
+                    this.start_position = this.driver.Position;
+                    this.timer1.Interval = Int32.Parse(this.textBox1.Text);
+
+                    this.listBox1.Items.Add("Started at " + DateTime.Now.ToShortTimeString() + " with Interval " + this.timer1.Interval.ToString());
+                    this.timer1.Enabled = true;
+                    this.button1.Text = "Disable Stresstest";
+                }
+                else
+                {
+                    this.timer1.Enabled = false;
+                    this.button1.Text = "Enable Stresstest";
+                }
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (this.IsConnected)
+            {
+                count_ticks++;
+                try
+                {
+
+
+                    var tmp = this.driver.IsMoving;
+                    var tmp2 = this.driver.CanReverse;
+                    var tmp3 = this.driver.Position;
+                    var tmp4 = this.driver.StepSize;
+
+
+                    if (count_ticks >= 10)
+                    {
+                        if (this.checkBox1.Checked && this.driver.IsMoving)
+                        {
+                            if (this.waiting_for_move == false)
+                            {
+
+
+                                this.listBox1.Items.Add("Still moving, will not send a new move command");
+                            }
+
+                            this.waiting_for_move = true;
+                        }
+                        else
+                        {
+                            this.waiting_for_move = false;
+
+                            float new_position;
+                            // Move position
+                            if (this.driver.Position > this.start_position)
+                            {
+                                new_position = this.start_position -10;
+                            }
+                            else
+                            {
+                                new_position = this.start_position + 10;
+                            }
+                            if (new_position > 360)
+                            {
+                                new_position = 360;
+                            } else if (new_position < 0)
+                            {
+                                new_position = 0;
+                            }
+                            // ToDo: Change StepSize
+
+                            this.driver.MoveAbsolute(new_position);
+                            count_ticks = 0;
+                            this.listBox1.Items.Add(DateTime.Now.ToLongTimeString() + ": Position: " + this.driver.Position.ToString() + ", IsMoving: " + tmp.ToString() + ", CanReverse: " + tmp2.ToString() + ", StepSize: " + tmp3.ToString() + " ( done 10 calls in between)");
+                        }
+                    }
+                    this.total_calls++;
+                }
+                catch (Exception ex)
+                {
+                    this.timer1.Enabled = false;
+                    this.listBox1.Items.Add(ex.Message + " (total calls " + this.total_calls.ToString() + ")");
+                }
+
+                this.listBox1.SelectedIndex = this.listBox1.Items.Count - 1;
+
+                // Move position up a little
+            }
+        }
+    }
+}
